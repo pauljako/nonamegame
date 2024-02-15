278e21e65777e18ae199d2e6c5f851720603a518
pauljako <paulifey18@gmail.com>
2024-02-15 12:10:08 +0100

    Made it functional

--- launcher.py
+++ launcher.py
@@ -1,32 +1,52 @@
 #!/bin/python3
 import nogamename
 import customtkinter
+import threading
+
+entry: customtkinter.CTkEntry
+label: customtkinter.CTkLabel
 
 
 def set_status(status: str):
+    global label
     label.configure(text=f"Status: {status}")
 
 
-def start_callback():
+def start():
+    global entry
     if entry.get() == "":
         set_status("Please Enter Username")
     else:
-        print(entry.get())
+        set_status("Initializing")
+        nogamename.init((740, 600), f"NoGameName (Player: {entry.get()})", "Times New Roman", 12, entry.get())
+        set_status("Running")
+        nogamename.main()
+        set_status("Ready")
 
 
-app = customtkinter.CTk()
-app.title("NoNameGame Launcher")
-app.geometry("350x200")
-app.grid_columnconfigure(0, weight=1)
+def start_callback():
+    run_thread = threading.Thread(target=start)
+    run_thread.start()
 
 
-entry = customtkinter.CTkEntry(app, placeholder_text="Username")
-entry.grid(row=0, column=0, padx=20, pady=20, sticky="ew")
-button = customtkinter.CTkButton(app, text="Start", command=start_callback)
-button.grid(row=1, column=0, padx=20, pady=20, sticky="ew")
-label = customtkinter.CTkLabel(app, text="")
-label.grid(row=3, column=0, padx=20, pady=20, sticky="ew")
+def main():
+    global entry, label
+    app = customtkinter.CTk()
+    app.title("NoNameGame Launcher")
+    app.geometry("350x200")
+    app.grid_columnconfigure(0, weight=1)
 
-set_status("Ready")
+    entry = customtkinter.CTkEntry(app, placeholder_text="Username")
+    entry.grid(row=0, column=0, padx=20, pady=20, sticky="ew")
+    button = customtkinter.CTkButton(app, text="Start", command=start_callback)
+    button.grid(row=1, column=0, padx=20, pady=20, sticky="ew")
+    label = customtkinter.CTkLabel(app, text="")
+    label.grid(row=3, column=0, padx=20, pady=20, sticky="ew")
 
-app.mainloop()
+    set_status("Ready")
+
+    app.mainloop()
+
+
+if __name__ == '__main__':
+    main()
