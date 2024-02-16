0bf96dcbf3fcb0235f4178a404d8a249c1046148
pauljako <paulifey18@gmail.com>
2024-02-16 08:25:47 +0100

    Now saves Username in config file

--- launcher.py
+++ launcher.py
@@ -1,10 +1,40 @@
 #!/bin/python3
+import json
+import os.path
+
 import nogamename
 import customtkinter
 import threading
 
 entry: customtkinter.CTkEntry
 label: customtkinter.CTkLabel
+config: dict
+
+
+def save_config(new_config: dict):
+    config_path = "config.json"
+    with open(config_path, "w") as f:
+        f.write(json.dumps(new_config))
+
+
+def load_config() -> dict:
+    config_path = "config.json"
+    config: dict
+    defaults = {
+        "name": "",
+        "color": "black"
+    }
+    if not os.path.exists(config_path):
+        config = defaults
+    else:
+        with open(config_path, "rb") as f:
+            config = json.load(f)
+    for req_objects in defaults.keys():
+        if req_objects not in config:
+            print(f"Warning: Corrupt Config File: no '{req_objects}' ")
+            config[req_objects] = defaults[req_objects]
+    print(config)
+    return config
 
 
 def set_status(status: str):
@@ -13,12 +43,13 @@ def set_status(status: str):
 
 
 def start():
-    global entry
+    global entry, config
     if entry.get() == "":
         set_status("Please Enter Username")
     else:
+        config["name"] = entry.get()
         set_status("Initializing")
-        nogamename.init((740, 600), f"NoGameName (Player: {entry.get()})", "Times New Roman", 12, entry.get())
+        nogamename.init((740, 600), f"NoGameName (Player: {config['name']})", "Times New Roman", 12, config['name'])
         set_status("Running")
         nogamename.main()
         set_status("Ready")
@@ -30,22 +61,28 @@ def start_callback():
 
 
 def main():
-    global entry, label
+    global entry, label, config
     app = customtkinter.CTk()
-    app.title("NoNameGame Launcher")
-    app.geometry("350x200")
-    app.grid_columnconfigure(0, weight=1)
+    app.title("NoGameName Launcher")
+    app.geometry("350x250")
+    app.columnconfigure(0, weight=1)
+    app.grid_rowconfigure(0, weight=1)
 
-    entry = customtkinter.CTkEntry(app, placeholder_text="Username")
-    entry.grid(row=0, column=0, padx=20, pady=20, sticky="ew")
+    config = load_config()
+
+    status_frame = customtkinter.CTkFrame(app)
+    status_frame.grid(row=3, column=0, padx=20, pady=20, sticky="ew")
+    entry = customtkinter.CTkEntry(app, placeholder_text="Username", textvariable=customtkinter.StringVar(value=config["name"]))
+    entry.grid(row=0, column=0, padx=20, pady=20, sticky="new")
     button = customtkinter.CTkButton(app, text="Start", command=start_callback)
     button.grid(row=1, column=0, padx=20, pady=20, sticky="ew")
-    label = customtkinter.CTkLabel(app, text="")
-    label.grid(row=3, column=0, padx=20, pady=20, sticky="ew")
+    label = customtkinter.CTkLabel(status_frame, text="")
+    label.grid(row=1, column=0, padx=20, pady=20, sticky="ew")
 
     set_status("Ready")
 
     app.mainloop()
+    save_config(config)
 
 
 if __name__ == '__main__':
