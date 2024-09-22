71b1135b8fe8151b8986e028dfba7b2dde24562a
pauljako <paulifey18@proton.me>
2024-09-22 13:33:44 +0200

    Added Support for Rooms

--- server/server.py
+++ server/server.py
@@ -1,38 +1,44 @@
+#!/usr/bin/env python3
+
 import dill
 import socket
 from threading import Thread
 import sys
 import pygame
 
-SERVER = ""
-PORT = 25565
-
-players = {}
+rooms = {}
 
 
-def threaded_client(connection: socket.socket, username: str, player: pygame.Rect, color, scene):
-    global players
+def threaded_client(connection: socket.socket, username: str, player: pygame.Rect, color, scene, room_code: str):
+    global rooms
+    if room_code not in rooms:
+        print(f"Creating new Room with Code: {room_code}")
+        rooms[room_code] = {}
+    players = rooms[room_code]
     players[username] = player
     players[username + ".color"] = color
     players[username + ".scene"] = scene
+    connection.send(dill.dumps(values.get_values()))
     connection.send(dill.dumps(players))
-    reply = ""
 
     while True:
         try:
             data = dill.loads(connection.recv(4096))
 
             if not data:
-                print("Disconnected")
+                print(f" {username} Disconnected")
                 break
             elif data[0] == "color":
                 players[username + ".color"] = data[1]
+                rooms[room_code] = players
                 connection.sendall(dill.dumps(players))
             elif data[0] == "scene":
                 players[username + ".scene"] = data[1]
+                rooms[room_code] = players
                 connection.sendall(dill.dumps(players))
             else:
                 players[username] = data
+                rooms[room_code] = players
                 connection.sendall(dill.dumps(players))
         except:
             print("Error")
@@ -42,14 +48,19 @@ def threaded_client(connection: socket.socket, username: str, player: pygame.Rec
     players.pop(username)
     players.pop(username + ".color")
     players.pop(username + ".scene")
+    if len(players) == 0:
+        print(f"Closing Room {room_code}")
+        rooms.pop(room_code)
+    else:
+        rooms[room_code] = players
     connection.close()
 
 
-def main():
+def main(address, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     try:
-        s.bind((SERVER, PORT))
+        s.bind((address, port))
     except socket.error as error:
         print(f"Error {error}")
 
@@ -68,11 +79,20 @@ def main():
             player = connection_data[2]
             color = connection_data[3]
             scene = connection_data[4]
-            print(f"Connected to: {address} Username: {username} Color: {color}")
-            Thread(target=threaded_client, args=(connection, username, player, color, scene)).start()
+            room_code = connection_data[5]
+            print(f"Connected to: {address} Username: {username}, Color: {color}, Scene: {scene}, Room Code: {room_code}")
+            Thread(
+                target=threaded_client,
+                args=(connection, username, player, color, scene, room_code),
+                name=f"NoGameName Server {address}",
+                daemon=True
+            ).start()
         else:
             print(f"Cannot connect to: {address}")
 
 
 if __name__ == "__main__":
-    main()
+    import values
+    main("127.0.0.1", 25565)
+else:
+    from server import values
