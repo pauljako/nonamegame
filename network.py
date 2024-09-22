6e8b62cc64add175fb65c03bb3cf1a31d8e7f8ba
pauljako <paulifey18@proton.me>
2024-09-22 13:32:55 +0200

    Update

--- network.py
+++ network.py
@@ -1,28 +1,32 @@
 import dill
 import socket
 import pygame
+import errno
 
 
 class Network:
-    def __init__(self, username: str, player: pygame.Rect, color, scene: int = 0):
+    def __init__(self, username: str, player: pygame.Rect, color, room_code: str, scene: int, address: str, port: int):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         # self.server = "node2.craftsblock.eu"
-        self.server = "127.0.0.1"
-        self.port = 25565
+        self.server = address
+        self.port = port
         self.buffersize = 4096
         self.address = (self.server, self.port)
         self.username = username
         self.player = player
         self.color = color
         self.scene = scene
+        self.room_code = room_code
 
     def connect(self):
         try:
             self.client.connect(self.address)
-            return self.send(["game", self.username, self.player, self.color, self.scene])
+            new_values = self.send(["game", self.username, self.player, self.color, self.scene, self.room_code])
+            new_players = dill.loads(self.client.recv(self.buffersize))
+            return new_values, new_players
         except:
             print("Connection Error")
-            return ""
+            return None, None
 
     def send(self, data):
         try:
@@ -31,3 +35,17 @@ class Network:
         except socket.error as error:
             print(f"Error: {error}")
             return ""
+
+
+def check_port(port) -> bool:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        s.bind(("127.0.0.1", port))
+    except socket.error as e:
+        if e.errno == errno.EADDRINUSE:
+            return False
+        else:
+            print(e)
+            return False
+    s.close()
+    return True
