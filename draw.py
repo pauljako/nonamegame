2933627b8a7ccd29759dc47d4b19a110049a400e
pauljako <paulifey18@proton.me>
2024-09-22 13:32:41 +0200

    Bug Fix & Positioning Offset

--- draw.py
+++ draw.py
@@ -1,7 +1,17 @@
 import pygame
 
 
-def draw(players: dict, ground, scene: int, window: pygame.Surface, offline: bool, font: pygame.font, assets: dict):
+def draw(
+        players: dict,
+        ground,
+        scene: int,
+        window: pygame.Surface,
+        offline: bool,
+        font: pygame.font,
+        assets: dict,
+        x_offset: float,
+        y_offset: float
+):
     window.fill("white")
     rects = {}
     colors = {}
@@ -13,16 +23,16 @@ def draw(players: dict, ground, scene: int, window: pygame.Surface, offline: boo
         for d in range(int(i.size[0] / GROUND_X_SIZE)):
             f = d * GROUND_X_SIZE
             # window.blit(pygame.transform.scale(assets["ground"], (i.size[0] + f, i.size[1])), i)
-            window.blit(assets["ground"], (i.x + f, i.y))
+            window.blit(assets["ground"], (i.x + f + x_offset, i.y + y_offset))
         # pygame.draw.rect(window, "green", i)
     for n, p in players.items():
         if isinstance(p, pygame.Rect):
             rects[n] = p
-        else:
+        elif n.split(".")[1] == "color":
             na = n.split(".")[0]
             colors[na] = p
     for n, p in rects.items():
-        pygame.draw.rect(window, colors[n], p)
+        window.blit(pygame.Surface((p.width, p.height)), (p.x + x_offset, p.y + y_offset))
     pygame.display.update()
 
 
